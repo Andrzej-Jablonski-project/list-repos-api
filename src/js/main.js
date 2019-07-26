b4aeeed26329a6f7d81f47fa5eb5ed166ca573b6
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-07-26 12:19:52 +0200

    feat: repository list on github

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,16 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const reposList = document.querySelector('.repos__list--js');
 
-console.log(`Hello world!`);
-
-
+fetch('https://api.github.com/users/Andrzej-Jablonski-project/repos?sort=created')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for (const repo of repos) {
+      reposList.innerHTML += `<li class="repos__name"><a class="repos__link" href="${repo.html_url}">${repo.name}</a></li >`;
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
